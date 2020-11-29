49b2fd97535c2cf1a4ed77994b33628b97d2bc62
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2020-11-29 13:31:54 +0200

    Update js-gallery.js

--- src/js/js-gallery.js
+++ src/js/js-gallery.js
@@ -1,14 +1,20 @@
-import array from "../gallery-items.js";
+import galleryArray from "../gallery-items.js";
 
 const refs = {
   gallery: document.querySelector(".js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  btnCloseModal: document.querySelector('button[data-action="close-lightbox"]'),
+  modal: document.querySelector(".lightbox__overlay"),
+  lightboxContent: document.querySelector(".lightbox__content"),
+  lightboxImage: document.querySelector(".lightbox__image"),
 };
-const getImgRef = array.map((item, i) => {
+galleryArray.map((item, i) => {
   let img = document.createElement("img");
   img.classList.add("gallery__image");
   img.setAttribute("src", item.preview);
   img.setAttribute("data-source", item.original);
-  img.setAttribute("data-intex", i);
+  img.setAttribute("alt", item.description);
+  img.setAttribute("data-index", i);
   let a = document.createElement("a");
   a.classList.add("gallery__link");
   a.setAttribute("href", item.original);
@@ -16,22 +22,80 @@ const getImgRef = array.map((item, i) => {
   let li = document.createElement("li");
   li.classList.add("gallery__item");
   li.append(a);
-  return li;
+  refs.gallery.append(li);
 });
 
-refs.gallery.append(...getImgRef);
+//refs.gallery.append(...getImgRef);
 
 //console.log(getImgRef);
 
 refs.gallery.addEventListener("click", onGalleryClick);
+refs.btnCloseModal.addEventListener("click", closeModal);
+refs.modal.addEventListener("click", onModalClickClose);
+//refs.lightboxContent.addEventListener("click", onClickModal);
+window.addEventListener("keydown", onPressLeft);
+window.addEventListener("keydown", onPressRight);
 
 function onGalleryClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-
-  const imgRef = event.target;
-  const largeImageURL = imgRef.dataset.source;
-  console.log(largeImageURL);
+  if (event.target.nodeName === "IMG") {
+    window.addEventListener("keydown", onPressEsc);
+    const imgRef = event.target;
+    const largeImageURL = imgRef.dataset.source;
+    const imgAlt = imgRef.alt;
+    refs.lightbox.classList.add("is-open");
+    refs.lightboxImage.setAttribute("src", largeImageURL);
+    refs.lightboxImage.setAttribute("alt", imgAlt);
+    console.log(imgRef.dataset);
+  }
 }
+
+function closeModal() {
+  window.removeEventListener("keydown", onPressEsc);
+  refs.lightbox.classList.remove("is-open");
+  refs.lightboxImage.src = "";
+  refs.lightboxImage.alt = "";
+}
+
+function onModalClickClose(event) {
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+}
+
+function onPressEsc(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
+}
+
+function setImgModAttr(step, index) {
+  refs.lightboxImage.src = galleryArray[index + step].original;
+  refs.lightboxImage.alt = galleryArray[index + step].description;
+  refs.lightboxImage.dataset.index = `${index + step}`;
+}
+
+function onPressLeft(event) {
+  if (event.code === "ArrowLeft") {
+    const index = refs.lightboxImage.dataset.index;
+    console.log(refs.lightboxImage.dataset.index);
+    if (index === 0) return;
+    setImgModAttr(-1, index);
+  }
+}
+
+function onPressRight(event) {
+  if (event.code === "ArrowRight") {
+    const index = Number(refs.lightboxImage.dataset.index);
+    if (index === galleryArray.length - 1) return;
+    setImgModAttr(1, index);
+  }
+}
+
+console.log(galleryArray[1].original);
+// function onClickModal (step, index) {
+//   if (){}
+// }
